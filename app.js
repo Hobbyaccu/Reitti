e852712284d0e012e0893f6d5e54611e3992374d
Hobbyaccu <vitunpiri@gmail.com>
2026-03-19 16:41:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
-// app.js
-
-// Import modules (if needed)
-// const module = require('module-name');
-
-// Main application code
-function main() {
-    console.log('Application started');
-}
-
-// Run application
-main();
-
 function showSatelliteMap() {
     const mapContainer = document.getElementById('map');
     if (mapContainer) {
-        mapContainer.innerHTML = '<iframe src="https://maps.google.com/?output=embed" width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy"></iframe>';
+        mapContainer.innerHTML = 
+            '<iframe src="https://maps.google.com/?output=embed" ' +
+            'width="100%" height="500" style="border:0;" ' +
+            'allowfullscreen="" loading="lazy"></iframe>';
     }
 }
-
-// Expose function to global scope for HTML button
-window.showSatelliteMap = showSatelliteMap;
