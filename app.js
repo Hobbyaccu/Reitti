d2e4959b32ecdff0adf912de084a711b78e19145
Hobbyaccu <vitunpiri@gmail.com>
2026-03-19 16:56:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-function showSatelliteMap() {
-    const mapContainer = document.getElementById('map');
-    if (mapContainer) {
-        mapContainer.innerHTML = 
-            '<iframe src="https://maps.google.com/?output=embed" ' +
-            'width="100%" height="500" style="border:0;" ' +
-            'allowfullscreen="" loading="lazy"></iframe>';
-    }
-}
-
 function showStreetView() {
     const mapContainer = document.getElementById('map');
     if (mapContainer) {
