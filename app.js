39dd97f9a3ed68f4caf2ce03f7d2bcb72ae60dfe
Hobbyaccu <vitunpiri@gmail.com>
2026-03-19 16:38:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -10,3 +10,13 @@ function main() {
 
 // Run application
 main();
+
+function showSatelliteMap() {
+    const mapContainer = document.getElementById('map');
+    if (mapContainer) {
+        mapContainer.innerHTML = '<iframe src="https://maps.google.com/?output=embed" width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy"></iframe>';
+    }
+}
+
+// Expose function to global scope for HTML button
+window.showSatelliteMap = showSatelliteMap;
